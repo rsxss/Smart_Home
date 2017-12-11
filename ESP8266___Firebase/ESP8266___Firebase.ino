86fc454c3527330f7e660b4161769f96b4006bca
Natworpong Loyswai <natworpong.l@om4you.com>
2017-12-11 13:51:17 +0700

    On branch master 12/11/2017  Your branch is up-to-date with 'origin/master'.

--- ESP8266___Firebase/ESP8266___Firebase.ino
+++ ESP8266___Firebase/ESP8266___Firebase.ino
@@ -1,17 +1,20 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 #include <SoftwareSerial.h>
-#define WIFI_SSID       "MyASUS"
-#define WIFI_PASSWORD   "1ccccf2a7cd1"
+#define WIFI_SSID       "pengpond"
+#define WIFI_PASSWORD   "0865679871"
 #define FIREBASE_HOST "smarthomev01-626ae.firebaseio.com"
 #define FIREBASE_AUTH "Db0D7jIrZEutBdE8qhj03t92QPcasT8xw3CKUlv8"
 SoftwareSerial esp(D5,D6);
 String a;
+String e;
 char b;
+int c;
+float d;
 void setup() {
   Serial.begin(9600);
   esp.begin(9600);
-  /*Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP());
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
@@ -23,42 +26,48 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Firebase.setInt("/Sensor/Sensor_val", 0);*/
+  //Firebase.setInt("/Sensor/Sensor_val", 0);
 }
 
-void loop() {
-  while(esp.available()){
+void get_nucleo(){
+   while(esp.available()){
     b = esp.read();
-    delay(50);
-    Serial.print("b:");
-    delay(50);
-    Serial.println(b);
-    delay(50);
     if (b == '\n'){
-      Serial.println("BREAK");
       break;
     }
     else{
       a = a + b;
-      delay(50);
-      Serial.println(a);
-      delay(50);
+      //Serial.println(a);
     }
   }
   Serial.println(a);
-  delay(50);
-  a = "";
-  delay(3000);
-  //Serial.println("end");
-  /*esp.p1rintln(Firebase.getInt("Sensor/Sensor_val"));
-  delay(2000);
-  digitalWrite(ledPin, Firebase.getInt("LED"));
-  delay(500);
-  if (esp.available()){
-    //Serial.println("esp readable");
-    esp.write("B");
-    Firebase.setInt("/Sensor/Sensor_val", esp.read());
-    Serial.print("nucleo_data: ");
-    Serial.println(char(esp.read()));
-  }*/
 }
+
+void check_get_nucleo(){
+  if (a.charAt(0) == 'r'){
+    e = a.charAt(2);
+    c = e.toInt();
+    Serial.println(c);
+    Firebase.setInt("/Sensor/Rain",c);
+  }
+  else if(a.charAt(0) == 't'){
+    e = a.substring(2);
+    d = e.toFloat();
+    Firebase.setFloat("/Sensor/Temp",d);    
+  }
+  a = "";
+  delay(50);
+}
+
+void get_firebase(){
+  
+}
+
+void loop() {
+  get_nucleo();
+  delay(50);
+  check_get_nucleo();
+  delay(50);
+}
+
+
