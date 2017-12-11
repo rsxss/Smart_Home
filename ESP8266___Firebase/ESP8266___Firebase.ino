fdc562a783543afe168118605ea6009e85414e0f
Natworpong Loyswai <Natworpong.L@om4you.com>
2017-12-11 19:29:51 +0700

    7:29 11-Dec-17

--- ESP8266___Firebase/ESP8266___Firebase.ino
+++ ESP8266___Firebase/ESP8266___Firebase.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 #include <SoftwareSerial.h>
-#define WIFI_SSID       "pengpond"
-#define WIFI_PASSWORD   "0865679871"  
+#define WIFI_SSID       "MyASUS"
+#define WIFI_PASSWORD   "1ccccf2a7cd1"  
 #define FIREBASE_HOST "smarthomev01-626ae.firebaseio.com"
 #define FIREBASE_AUTH "Db0D7jIrZEutBdE8qhj03t92QPcasT8xw3CKUlv8"
 SoftwareSerial esp(D5,D6);
@@ -40,34 +40,64 @@ void get_nucleo(){
       //Serial.println(a);
     }
   }
-  Serial.println(a);
+  //Serial.println(a);
 }
 
 void check_get_nucleo(){
   if (a.charAt(0) == 'r'){
     e = a.charAt(2);
     c = e.toInt();
-    Serial.println(c);
+    //Serial.println(c);
     Firebase.setInt("/Sensor/Rain",c);
   }
   else if(a.charAt(0) == 't'){
     e = a.substring(2);
     d = e.toFloat();
-    Firebase.setFloat("/Sensor/Temp",d);    
+    Firebase.setFloat("/Sensor/Temp_O",d);    
+  }
+  else if(a.charAt(0) == 'I'){
+    String text = "/RFID/" + a.substring(2);
+    Firebase.setString(text,a.substring(2));
+  }
+  else if(a.charAt(0) == 'C'){
+    String text = "/RFID/" + a.substring(2);
+    String result = Firebase.getString(text);
+    if (result){
+      esp.print('1');
+    }
+    else esp.print('0');
+  }
+  else if(a.charAt(0) == 'D'){
+    String text = "/RFID/" + a.substring(2);
+    Firebase.remove(text);
+  }
+  else if(a.charAt(0) == 'L'){
+    String text = "/Log/" + a.substring(2);
+    
   }
   a = "";
   delay(50);
 }
 
 void get_firebase(){
+    /*Firebase.stream("Setting/Rain_status");
+    if (Firebase.available()){
+      FirebaseObject event = Firebase.readEvent();
+      String type = event.getString("type");
+      String path = event.getString("path");
+      String data = event.getString("data"); 
 
+    }*/
 }
+
+
 
 void loop() {
   get_nucleo();
   delay(50);
   check_get_nucleo();
   delay(50);
+  //get_firebase();
 }
 
 
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Administrater\Desktop\Smart_Home\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
